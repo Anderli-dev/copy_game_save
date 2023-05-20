7af1353d776a7bf5dfe8dd016f0dbfeadf3a7119
Ivan Suprun <ivan.suprun2015@gmail.com>
2023-05-20 06:54:34 +0300

    docs(): write comments and update readme
    Added comments to the main.py file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Copy hoi4 save
-This is a little program to save a previous save when the current save is damaged
+This is a little program to save a previous save file when the current save file is damaged
+
+If you need help wright me
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 
 from check_if_game_running import check_if_process_running
 from path import original, target
+    # original - path to the file to be copied
+    # target - path to where it will be copied (with the name of the copy file)
 
 if __name__ == '__main__':
 
@@ -12,17 +14,20 @@ if __name__ == '__main__':
         while True:
             if check_if_process_running('hoi4'):
                 current_modified_time = os.path.getmtime(original)
+                # checking  if need to copy by creation time
                 if current_modified_time > last_modified_time:
                     time.sleep(20)
                     file_stats = os.stat(original)
+                    # if file is damaged (weight 0 bits), deletes it and does not copy it
                     if file_stats.st_size == 0:
                         os.remove(original)
                     else:
+                    # if file is okay, it will wait until the game writes all data and copies it
                         time.sleep(30)
                         shutil.copyfile(original, target)
 
                     last_modified_time = current_modified_time
             else:
-                time.sleep(60)
+                time.sleep(90)
     except KeyboardInterrupt:
         print('\n')
